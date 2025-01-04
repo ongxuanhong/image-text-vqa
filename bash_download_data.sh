1e77e96097a6c7203a199b33784c23b0eebb6987
hongong <hongong>
2025-01-04 15:42:11 +0700

    Download data

---
+++ bash_download_data.sh
@@ -0,0 +1,20 @@
+# Install gdown if not already installed
+pip install gdown
+
+# Create data directory if it doesn't exist
+mkdir -p data
+
+# Download the file using gdown
+gdown "https://drive.google.com/uc?id=1zRHT9eSFUNK6o3AHwQmyAeFzXfEzkEts" -O data/dataset.zip
+
+# Unzip the file into the data directory
+unzip data/dataset.zip -d data/
+
+# Optional: Remove the zip file after extraction
+rm data/dataset.zip
+
+# Create notebooks directory if it doesn't exist
+mkdir -p notebooks
+
+# Download entire folder using gdown
+gdown https://drive.google.com/drive/folders/1rP8W1skBcLVH0Vxq7ELO40iDmLyVmpDz -O notebooks/ --folder
